09fd2b15726dd8ee88498cdb98cfa8de099af61f
SaurabPoudel <poudelsaurab20@gmail.com>
2023-12-02 14:20:44 +0545

    sidebar

--- src/_root/pages/RootLayout.tsx
+++ src/_root/pages/RootLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
-import {Nav} from "../../nav"
 import Topbar from "@/components/shared/Topbar"
+import LeftSideBar from "@/components/shared/LeftSideBar"
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
       <Topbar/>
-      <Nav/>
+      {/* <Nav/> */}
+      <LeftSideBar/>
       <section className="flex flex-1 h-full">
         <Outlet/>
       </section>
